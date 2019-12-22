3b37b8afc3e230544e78c86ab7877fbcafabb8a7
Andrew Azores <me@andrewazor.es>
2019-12-22 00:10:34 -0500

    Remove unused module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { NgbModule } from '@ng-bootstrap/ng-bootstrap'
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -14,7 +13,6 @@ import { AppComponent } from './app.component';
     BrowserModule,
     AppRoutingModule,
     FormsModule,
-    NgbModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
