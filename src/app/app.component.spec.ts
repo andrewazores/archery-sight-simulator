90c66264f35cee071bb0cf46578b650c8fe3080c
Andrew Azores <me@andrewazor.es>
2019-12-21 19:55:42 -0500

    Add basic right-column parameter controls

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -19,17 +19,4 @@ describe('AppComponent', () => {
     const app = fixture.debugElement.componentInstance;
     expect(app).toBeTruthy();
   });
-
-  it(`should have as title 'archery-sight-simulator'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('archery-sight-simulator');
-  });
-
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('archery-sight-simulator app is running!');
-  });
 });
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.less']
 })
 export class AppComponent {
-  title = 'archery-sight-simulator';
+
+  targetDistance = 70;
+  targetDiameter = 122;
+  eyePinDistance = 100;
+  apertureDiameter = 10;
+
 }
